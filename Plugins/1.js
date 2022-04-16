b22c8e14971af426295a03d5da16496d08ca2093
netLingSky <98532881+netLingSky@users.noreply.github.com>
2022-04-17 01:05:48 +0800

    Create 1.js

---
+++ Plugins/1.js
@@ -0,0 +1,3 @@
+var FontStyle=document.createElement("style")
+FontStyle.innerHTML="*{font-weight:bold !important;text-shadow:1px 2px 5px #b2b2b2 !important;font-family:'Microsoft YaHei';}";
+document.head.append(FontStyle);
